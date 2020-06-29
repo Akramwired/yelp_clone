2b02122d66532afa8bd97e7e8518937aee129067
Akramwired <kahd.unplugged@gmail.com>
2020-06-29 20:08:40 +0600

    yelp search corrected

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,9 @@
 const apiKey=process.env.REACT_APP_API_KEY;
 const Yelp={
     searchYelp(term, location, sortBy){
-        return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}',{
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
             headers:{
-                Authorization: 'Bearer ${apiKey}'
+                Authorization: `Bearer ${apiKey}`
             }
         }).then((response) => {
             return response.json();
